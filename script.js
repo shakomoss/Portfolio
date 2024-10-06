78761ef4d89635be0ad6fbd2f17e1334cf57fb9b
shakomoss <Mosiashvilishalva@gmail.com>
2024-10-06 19:43:12 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,162 @@
+const ehtemele = document.getElementById("chtml"); // Access the first element
+
+const transform = () => {
+    ehtemele.style.width = "200px"; 
+    ehtemele.style.height = "200px";
+    ehtemele.style.opacity = "100%"
+};
+
+const back = () => {
+    ehtemele.style.width = "100px"; 
+    ehtemele.style.height = "100px";
+    ehtemele.style.opacity = "50%"
+
+}
+ehtemele.addEventListener("mouseenter", transform);
+ehtemele.addEventListener("mouseleave", back);
+
+
+const cies = document.getElementById("css"); // Access the first element
+
+const transformeri = () => {
+    cies.style.width = "200px"; 
+    cies.style.height = "200px";
+    cies.style.opacity = "100%"
+};
+
+const backeri = () => {
+    cies.style.width = "100px"; 
+    cies.style.height = "100px";
+    cies.style.opacity = "50%"
+    
+
+}
+cies.addEventListener("mouseenter", transformeri);
+cies.addEventListener("mouseleave", backeri);
+
+const jeies = document.getElementById("js"); // Access the first element
+
+const transformori = () => {
+    jeies.style.width = "200px"; 
+    jeies.style.height = "200px";
+    jeies.style.opacity = "100%"
+};
+
+const backori = () => {
+    jeies.style.width = "100px"; 
+    jeies.style.height = "100px";
+    jeies.style.opacity = "50%"
+    
+
+}
+jeies.addEventListener("mouseenter", transformori);
+jeies.addEventListener("mouseleave", backori);
+
+const reaqt = document.getElementById("react"); // Access the first element
+
+const transformsri = () => {
+    reaqt.style.width = "200px"; 
+    reaqt.style.height = "200px";
+    reaqt.style.opacity = "100%"
+};
+
+const backsri = () => {
+    reaqt.style.width = "100px"; 
+    reaqt.style.height = "100px";
+    reaqt.style.opacity = "50%"
+    
+
+}
+reaqt.addEventListener("mouseenter", transformsri);
+reaqt.addEventListener("mouseleave", backsri);
+
+const noud = document.getElementById("node"); // Access the first element
+
+const transformfor = () => {
+    noud.style.width = "200px"; 
+    noud.style.height = "200px";
+    noud.style.opacity = "100%"
+};
+
+const backfor = () => {
+    noud.style.width = "100px"; 
+    noud.style.height = "100px";
+    noud.style.opacity = "50%"
+    
+
+}
+noud.addEventListener("mouseenter", transformfor);
+noud.addEventListener("mouseleave", backfor);
+
+const eqspres = document.getElementById("express"); // Access the first element
+
+const transformfive = () => {
+    eqspres.style.width = "200px"; 
+    eqspres.style.height = "200px";
+    eqspres.style.opacity = "100%"
+};
+
+const backfive = () => {
+    eqspres.style.width = "100px"; 
+    eqspres.style.height = "100px";
+    eqspres.style.opacity = "50%"
+    
+
+}
+eqspres.addEventListener("mouseenter", transformfive);
+eqspres.addEventListener("mouseleave", backfive);
+
+
+const postgre = document.getElementById("postgre"); // Access the first element
+
+const transformsix = () => {
+    postgre.style.width = "200px"; 
+    postgre.style.height = "200px";
+    postgre.style.opacity = "100%"
+};
+
+const backsix = () => {
+    postgre.style.width = "100px"; 
+    postgre.style.height = "100px";
+    postgre.style.opacity = "50%"
+    
+
+}
+postgre.addEventListener("mouseenter", transformsix);
+postgre.addEventListener("mouseleave", backsix);
+
+const jit = document.getElementById("git"); // Access the first element
+
+const transformsev = () => {
+    jit.style.width = "200px"; 
+    jit.style.height = "200px";
+    jit.style.opacity = "100%"
+};
+
+const backsev = () => {
+    jit.style.width = "100px"; 
+    jit.style.height = "100px";
+    jit.style.opacity = "50%"
+    
+
+}
+jit.addEventListener("mouseenter", transformsev);
+jit.addEventListener("mouseleave", backsev);
+
+const shaudzo = document.getElementById("shago"); // Access the first element
+
+const win = () => {
+    shaudzo.style.width = "400px"; 
+    shaudzo.style.height = "400px";
+    
+};
+
+const ukan = () => {
+    shaudzo.style.width = "200px"; 
+    shaudzo.style.height = "200px";
+   
+    
+
+}
+shaudzo.addEventListener("mouseenter", win);
+shaudzo.addEventListener("mouseleave", ukan);
